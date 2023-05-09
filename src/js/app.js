ac880476ca93974518f0c0c8a3ad156610a5854c
cool-monsoon <102756118+cool-monsoon@users.noreply.github.com>
2023-05-09 16:56:48 +0300

    Regexes become constants

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,20 @@
 export default class Validator {
   validateUsername(userName) {
     this.userName = userName;
-    if (/[^\w\d-_]/.test(userName) === true) {
+    const latinLettersNubersDashUnderscoreAccepted = /[^\w\d-_]/;
+    const threeNumbersInRow = /\d{4}/;
+    const beginWithLetter = /^[\d-_]/;
+    const endWithLetter = /[\d-_]$/;
+    if (latinLettersNubersDashUnderscoreAccepted.test(userName) === true) {
       throw new Error('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
     }
-    if (/\d{4}/.test(userName) === true) {
+    if (threeNumbersInRow.test(userName) === true) {
       throw new Error('Имя не должно содержать подряд более трёх цифр');
     }
-    if (/^[\d-_]/.test(userName) === true) {
+    if (beginWithLetter.test(userName) === true) {
       throw new Error('Имя не должно начинаться цифрами, символами подчёркивания или тире');
     }
-    if (/[\d-_]$/.test(userName) === true) {
+    if (endWithLetter.test(userName) === true) {
       throw new Error('Имя не должно заканчиваться цифрами, символами подчёркивания или тире');
     }
     return true;
